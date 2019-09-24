80fdfabc4fe6618cf02f01e4ec2ef75280416074
ZOlesia <56olesia65@gmail.com>
2019-09-24 16:57:20 -0700

    Cloudinary photo upload added

--- API/Controllers/PhotosController.cs
+++ API/Controllers/PhotosController.cs
@@ -40,8 +40,18 @@ namespace API.Controllers
             _cloudinary = new Cloudinary(acc);
         }
 
+        [HttpGet("{id}", Name="GetPhoto")]
+        public async Task<IActionResult> GetPhoto(int id)
+        {
+            var photoFromRepo = await _repo.GetPhoto(id);
+
+            var photo = _mapper.Map<PhotoForReturnDto>(photoFromRepo);
+
+            return Ok(photo);
+        }
+
         [HttpPost]
-        public async Task<IActionResult> AddPhotoForUser(int userId, PhotoForCreationDto photoForCreationDto)
+        public async Task<IActionResult> AddPhotoForUser(int userId, [FromForm]PhotoForCreationDto photoForCreationDto)
         {
             if(userId != int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value))
                 return Unauthorized();
@@ -79,7 +89,8 @@ namespace API.Controllers
 
             if(await _repo.SaveAll())
             {
-                return Ok();
+                var photoToReturn = _mapper.Map<PhotoForReturnDto>(photo);
+                return CreatedAtRoute("GetPhoto", new { id = photo.Id}, photoToReturn);
             }
 
             return BadRequest("Could not add the photo");
