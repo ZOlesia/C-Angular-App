8e219e0f49cf08c518e6c45db7ec3b4d0d073d7f
ZOlesia <56olesia65@gmail.com>
2019-10-21 17:23:43 -0700

    modification

--- SPA/src/app/_guards/auth.guard.ts
+++ SPA/src/app/_guards/auth.guard.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
-import { Observable } from 'rxjs';
+import { CanActivate, Router } from '@angular/router';
 import { AuthService } from '../_services/auth.service';
 import { AlertifyService } from '../_services/alertify.service';
 
@@ -11,7 +10,7 @@ export class AuthGuard implements CanActivate {
   constructor(private authService: AuthService, private router: Router, private alertify: AlertifyService) {}
 
 
-  canActivate(): Observable<boolean> | Promise<boolean> | boolean {
+  canActivate(): boolean {
 
     if (this.authService.loggedin()) {
       return true;
