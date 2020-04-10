b96d216a5851a8b48750c2150d2fcea5b460cf59
ZOlesia <56olesia65@gmail.com>
2020-04-10 16:30:32 -0700

    added reactive form for register

--- API/Controllers/AuthController.cs
+++ API/Controllers/AuthController.cs
@@ -31,21 +31,18 @@ namespace API.Controllers
         [HttpPost("register")]
         public async Task<IActionResult> Register(UserForRegisterDto userForRegisterDto)
         {
-            //validate request if there is no [ApiController] after the namespace we would need to check if !Model.isValid ...
-
             userForRegisterDto.Username = userForRegisterDto.Username.ToLower();
 
             if (await _repo.UserExists(userForRegisterDto.Username))
                 return BadRequest("Username already exists");
 
-            var userToCreate = new User
-            {
-                Username = userForRegisterDto.Username
-            };
+            var userToCreate = _mapper.Map<User>(userForRegisterDto);
 
             var createdUser = await _repo.Register(userToCreate, userForRegisterDto.Password);
+            
+            var userToReturn = _mapper.Map<UserForDetailsDto>(createdUser);
 
-            return StatusCode(201);
+            return CreatedAtRoute("GetUser", new{Controller = "Users", id = createdUser.Id}, userToReturn);
         }
 
         [HttpPost("login")]
