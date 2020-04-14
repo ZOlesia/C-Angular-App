67538ec394d7af675df9d7008f1fc2daf08f197a
ZOlesia <56olesia65@gmail.com>
2020-04-13 19:26:07 -0700

    action filter added for the user LastActive

--- API/Startup.cs
+++ API/Startup.cs
@@ -63,6 +63,8 @@ namespace API
                     ValidateAudience = false
                 };
             });
+
+            services.AddScoped<LogUserActivity>(); //Action Filters for LastActive
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
