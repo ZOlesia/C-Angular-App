d97406766c50c25f07b176e772d8649049e88a53
ZOlesia <56olesia65@gmail.com>
2020-04-21 18:10:45 -0700

    likes added

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using API.Data;
 using API.Dtos;
 using API.Helpers;
+using API.Models;
 using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -69,6 +70,34 @@ namespace API.Controllers
                 return NoContent();
                 
             throw new Exception($"Updating user {id} failes on save");
+        }
+
+        [HttpPost("{id}/like/{recipientId}")]
+        public async Task<IActionResult> LikeUser(int id, int recipientId)
+        {
+            if(id != int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value))
+                return Unauthorized();
+            
+            var like = await _repo.GetLike(id, recipientId);
+
+            if(like != null)
+                return BadRequest("You have already liked this user");
+
+            if(await _repo.GetUser(recipientId) == null)
+                return NotFound();
+
+            like = new Like
+            {
+                LikerId = id,
+                LikeeId = recipientId
+            };
+
+            _repo.Add<Like>(like);
+
+            if(await _repo.SaveAll())
+                return Ok();
+
+            return BadRequest("Failed to like user");
         }
 
     }
--- API/Data/IDatingRepository.cs
+++ API/Data/IDatingRepository.cs
@@ -14,6 +14,7 @@ namespace API.Data
          Task<User> GetUser(int id);
          Task<Photo> GetPhoto(int id);
          Task<Photo> GetUserMainPhoto(int userId);
+         Task<Like> GetLike(int userId, int recipientId);
         
 
     }
